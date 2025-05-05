9e4baecc45ba8e47afd68b579f543fc886aa3270
Sara <154412036+sgargal@users.noreply.github.com>
2025-05-05 12:03:47 +0200

    empezado preguntas.php

---
+++ app/views/etc/preguntas.php
@@ -0,0 +1,46 @@
+<section class="faq">
+  <h2>Preguntas Frecuentes</h2>
+  <article class="faq-item">
+    <h3>¿Cómo reservo una cita?</h3>
+    <p>Puede reservar una cita clicando a la barbería donde desee realizar el servicio y una vez dentro elija el servicio que más se ajuste a usted. Podrá elegir el barbero y la hora disponible que prefiera.</p>
+  </article>
+
+  <article class="faq-item">
+    <h3>¿Necesito registrarme para pedir una cita?</h3>
+    <p>Sí, es necesario crear una cuenta para poder gestionar tus citas y recibir notificaciones.</p>
+  </article>
+
+  <article class="faq-item">
+    <h3>¿Puedo cancelar o modificar una cita?</h3>
+    <p>Sí, desde tu perfil puedes ver tus citas próximas y cancelarlas o modificarlas.</p>
+  </article>
+
+  <article class="faq-item">
+    <h3>¿Cómo puedo registrar mi barbería en GROOMY?</h3>
+    <p>Si tienes una barbería y quieres unirte a nuestra plataforma, contáctanos a través del <a href="contactanos.php"> formulario </a>del correo info@groomy.com</p>
+  </article>
+
+  <article class="faq-item">
+    <h3>¿Puedo ver el historial de mis citas anteriores?</h3>
+    <p>Sí, en tu perfil encontrarás una sección donde puedes consultar todas tus citas pasadas</p>
+  </article>
+
+  <article class="faq-item">
+    <h3>¿Se pueden hacer pagos a través de la aplicación?</h3>
+    <p>Sí, puedes pagar tus servicios directamente desde la web usando PayPal de forma rápida y segura.</p>
+  </article>
+
+  <article class="faq-contact">
+    <h3>¿Tienes otra pregunta?</h3>
+    <p>Si no has encontrado lo que buscabas, escríbenos directamente: </p>
+    <form>
+      <label for="email">Tu correo electrónico: </label>
+      <input type="email" id="email" name="email" required><br><br>
+
+      <label for="mensaje">Tu mensaje: </label>
+      <textarea id="mensaje" name="mensaje" required></textarea><br>
+
+      <button type="submit">Enviar</button>
+    </form>
+  </article>
+</section>
