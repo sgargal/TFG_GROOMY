f7bf80055da7130f06a597751a424ef4c65886fb
Sara <154412036+sgargal@users.noreply.github.com>
2025-05-05 12:20:09 +0200

    empezando contactanos.php

---
+++ app/views/etc/contactanos.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contáctanos - GROOMY</title>
+</head>
+<body>
+  <header>
+    <h1>CONTÁCTANOS</h1>
+  </header>
+  <section>
+    <h2>¿Tienes alguna pregunta?</h2>
+    <p class="contact-info">Puedes escribirnos directamente a nuestro correo: 
+      <a href="mailto:infogroomy@gmail.com"> infogroomy@gmail.com </a>
+    </p>
+
+    <p class="contact-info">O si prefieres, puedes llamarnos al teléfono: <strong> (+34) 650 092 423 </strong></p>
+
+    <p class="contact-info">También puedes encontrarnos en nuestras redes sociales: </p>
+    <section class="social-links">
+      <!-- Añadir redes sociales con iconos personalizados -->
+    </section>
+  </section>
+</body>
