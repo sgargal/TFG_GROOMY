ac11d288fd41a46010a1ba079a2e3d4408ce143c
Sara Garzón <saragarzongaldeano@gmail.com>
2025-05-13 16:45:14 +0200

    añadiendo css a modals-forms y faq

--- app/views/etc/preguntas.php
+++ app/views/etc/preguntas.php
@@ -1,5 +1,18 @@
-<section class="faq">
-  <h2>Preguntas Frecuentes</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Preguntas Frecuentes</title>
+  <link rel="icon" href="../../../assets/src/logoGROOMY-fondosin.png">
+  <link rel="stylesheet" href="../../../assets/css/style-etc.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>
+<body>
+  <buttom class="btn-back">
+    <a href="../../../public/index.php"><i class="fas fa-arrow-left"></i>Volver</a>
+  </buttom>
+  <h2>PREGUNTAS FRECUENTES</h2>
   <article class="faq-item">
     <h3>¿Cómo reservo una cita?</h3>
     <p>Puede reservar una cita clicando a la barbería donde desee realizar el servicio y una vez dentro elija el servicio que más se ajuste a usted. Podrá elegir el barbero y la hora disponible que prefiera.</p>
@@ -17,7 +30,7 @@
 
   <article class="faq-item">
     <h3>¿Cómo puedo registrar mi barbería en GROOMY?</h3>
-    <p>Si tienes una barbería y quieres unirte a nuestra plataforma, contáctanos a través del <a href="contactanos.php"><strong> formulario </strong></a>del correo info@groomy.com</p>
+    <p>Si tienes una barbería y quieres unirte a nuestra plataforma, contáctanos a través del <a href="contactanos.php"><strong> formulario </strong></a> o al correo info@groomy.com.</p>
   </article>
 
   <article class="faq-item">
@@ -43,4 +56,6 @@
       <button type="submit">Enviar</button>
     </form>
   </article>
-</section>
+</body>
+</html>
+
