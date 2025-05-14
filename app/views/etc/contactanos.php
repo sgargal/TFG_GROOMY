875dc2cdd4e12d5435dc9eaf2eadbb08a8a12294
Sara Garzón <saragarzongaldeano@gmail.com>
2025-05-14 14:22:19 +0200

    terminando contactanos.php

--- app/views/etc/contactanos.php
+++ app/views/etc/contactanos.php
@@ -4,22 +4,36 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Contáctanos - GROOMY</title>
+  <link rel="icon" href="../../../assets/src/logoGROOMY-fondosin.png">
+  <link rel="stylesheet" href="../../../assets/css/style-etc.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/vue@3/dist/vue.global.prod.js"></script>
 </head>
 <body>
   <header>
-    <h1>CONTÁCTANOS</h1>
+    <button class="btn-back">
+      <a href="../../../public/index.php"><i class="fas fa-arrow-left"></i>Volver</a>
+    </button>
+    <section class="faq-header">
+      <h1>CONTÁCTANOS</h1>
+    </section>
   </header>
-  <section>
+  <section class="faq-contact">
     <h2>¿Tienes alguna pregunta?</h2>
     <p class="contact-info">Puedes escribirnos directamente a nuestro correo: 
       <a href="mailto:infogroomy@gmail.com"> infogroomy@gmail.com </a>
     </p>
 
-    <p class="contact-info">O si prefieres, puedes llamarnos al teléfono: <strong> (+34) 650 092 423 </strong></p>
+    <p class="contact-info">O si prefieres, puedes llamarnos al teléfono: <strong> (+34) 900 000 000 </strong></p>
 
     <p class="contact-info">También puedes encontrarnos en nuestras redes sociales: </p>
     <section class="social-links">
-      <!-- Añadir redes sociales con iconos personalizados -->
+      <ul>
+        <li><a href="https://www.instagram.com/" target="_blank" id="socials"><i class="fa-brands fa-instagram"></i></a></li>
+        <li><a href="https://www.facebook.com/" target="_blank" id="socials"><i class="fa-brands fa-facebook"></i></a></li>
+        <li><a href="https://x.com/" target="_blank" id="socials"><i class="fa-brands fa-x"></i></a></li>
+        <li><a href="https://www.tiktok.com/" target="_blank" id="socials"><i class="fa-brands fa-tiktok"></i></a></li>
+      </ul>
     </section>
   </section>
 </body>
